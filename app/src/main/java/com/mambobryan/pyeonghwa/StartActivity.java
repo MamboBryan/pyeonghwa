b2ad1c16bfd0662854383f58945959da693be6d4
MamboBryan <mambobryan@gmail.com>
2019-04-05 11:08:00 +0300

    [Feature #165133954] Added puzzle logic

--- app/src/main/java/com/mambobryan/pyeonghwa/StartActivity.java
+++ app/src/main/java/com/mambobryan/pyeonghwa/StartActivity.java
@@ -10,18 +10,60 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class StartActivity extends AppCompatActivity {
 
-    RelativeLayout startLayout;
-
+    //Declare startActivity button
     Button letsGo;
+
+    //Declaring initial longPress state
+    int longPressed = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        //Initializing start activity button
+        letsGo = findViewById(R.id.lets_go_button);
+
+        //Method to solve the puzzle
+        letsGo.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                setRevealText(longPressed);
+                longPressed++;
+                return false;
+            }
+        });
+    }
+
+    /**
+     * This method sets the TextView text
+     *
+     * @param counter
+     */
+    public void setRevealText(int counter) {
+
+        if (counter == 0) {
+            Toast.makeText(this, "You've gotten a hint", Toast.LENGTH_SHORT).show();
+        } else if (counter == 1) {
+            TextView firstLetter = findViewById(R.id.start_puzzle_o);
+            firstLetter.setText("O");
+
+        } else if (counter == 2) {
+            TextView firstLetter = findViewById(R.id.start_puzzle_u);
+            firstLetter.setText("U");
+
+        } else if (counter == 3) {
+            TextView firstLetter = findViewById(R.id.start_puzzle_t);
+            firstLetter.setText("T");
+
+        } else {
+            Toast.makeText(this, "Too many long press", Toast.LENGTH_SHORT).show();
+        }
 
     }
 }
