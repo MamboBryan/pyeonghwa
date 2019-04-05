8c273f1653e9fd18074b475d8f0f46e6ba1981ee
MamboBryan <mambobryan@gmail.com>
2019-04-05 10:16:30 +0300

    [Feature #1615133954] Added poem and play button

--- app/src/main/java/com/mambobryan/pyeonghwa/StartActivity.java
+++ app/src/main/java/com/mambobryan/pyeonghwa/StartActivity.java
@@ -1,13 +1,27 @@
 package com.mambobryan.pyeonghwa;
 
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.TransitionDrawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class StartActivity extends AppCompatActivity {
+
+    RelativeLayout startLayout;
+
+    Button letsGo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        
     }
 }
